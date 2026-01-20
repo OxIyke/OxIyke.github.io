76b856441292357e16cb4df6606b5dad6bdbd416
Iyke <eokorie1911@gmail.com>
2026-01-20 16:32:59 +0100

    converted the project to next js

---
+++ valentines-next/app/yay/page.js
@@ -0,0 +1,14 @@
+export default function Yay() {
+    return (
+        <div className="container">
+            {/* Gif */}
+            <img src="/assets/images/togepi-and-pikachu.gif" id="togepi-gif" alt="a cute picture of togepi" />
+            {/* Question */}
+            <h1 id="message">
+                Thanks for being my Valentine! ⸜(｡˃ ᵕ ˂ )⸝♡
+            </h1>
+            
+            
+        </div>
+    );
+}
